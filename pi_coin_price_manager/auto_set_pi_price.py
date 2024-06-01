af40d44b60108136ac0af1a062040439f671f4f7
Mabalasik184 <137279667+Mabalasik184@users.noreply.github.com>
2024-06-01 09:11:48 +0800

    Update auto_set_pi_price.py

--- pi_coin_price_manager/auto_set_pi_price.py
+++ pi_coin_price_manager/auto_set_pi_price.py
@@ -7,9 +7,9 @@ def set_pi_coin_price(price):
     data = {"coin": "PI", "price": price}
     response = requests.post(url, headers=headers, json=data)
     if response.status_code == 200:
-        print("Pi coin price successfully set to $314.159")
+        print("Pi coin price successfully set to $314,159")
     else:
         print("Failed to set Pi coin price")
 
 # Set the Pi coin price to $314.159
-set_pi_coin_price(314.159)
+set_pi_coin_price(314,159)
